f0e4b036ca5b1efc259feefa912891d683147324
mayankbohra <bohramayank4u@gmail.com>
2023-05-24 00:18:31 +0530

    update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,7 +8,14 @@ const TextForm = (props) => {
     const handleLoClicked = () => {
         setText(text.toLowerCase());
     }
-
+    
+    const handleCopy = () => {
+        props.showAlert("Text copied successfully", "success");
+        const text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
@@ -18,14 +25,15 @@ const TextForm = (props) => {
     return (
         <>
         <div className="container">
-            <h1>{props.heading}</h1>
+            <h1 className={`text-${props.mode==='light'?'dark':'light'}`}>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClicked}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClicked}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClicked}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClicked}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
         </div>
-        <div className="container mt-4">
+        <div className={`container mt-4 text-${props.mode==='light'?'dark':'light'}`}>
             <h2>Your Text Summary</h2>
             <p>{text.trim()==='' ? 0 : text.trim().split(" ").length} words, {text.length} characters</p>
             <p>{0.008 * (text.trim()==='' ? 0 : text.trim().split(" ").length)} minutes read</p>
