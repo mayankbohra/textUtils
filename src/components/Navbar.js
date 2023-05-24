561ad11ae1067e6fb9e46175865360bce773922d
mayankbohra <bohramayank4u@gmail.com>
2023-05-24 20:28:50 +0530

    update title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 const Navbar = (props) => {
   return (
     <>
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} text-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <a className={`h2 text-decoration-none text-${props.mode==='light'?'dark':'light'}`} href="/">{props.title}</a>
         
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
